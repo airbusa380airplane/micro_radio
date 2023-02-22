93588ecd0271fa799bcaf0d4b6409ae5d76fc2d4
airbusa380airplane <123195899+airbusa380airplane@users.noreply.github.com>
2023-02-22 13:11:53 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,59 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 4) {
+        basic.showString("test-tex")
+    }
+    if (receivedNumber == 3) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . # . .
+            # # # # #
+            . . . . .
+            `)
+    }
+    if (receivedNumber == 2) {
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            `)
+    }
+    if (receivedNumber == 0) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    }
+    if (receivedNumber == 1) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(3)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(1)
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendNumber(2)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    radio.sendNumber(4)
+})
+basic.forever(function () {
+	
+})
